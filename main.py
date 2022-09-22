f21ceeeeacc82871805e9b3498cfeeb5145d9214
amir0901 <114090626+amir0901@users.noreply.github.com>
2022-09-22 15:12:29 +0545

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,37 @@ async def schedule_daily_message():
 
         await channel.send("Heyy Kiv have some water")
 
+async def schedule_daily_message2():
+    while True:
+        now = datetime.datetime.now()
+        #then = now+datetime.timedelta(days=0)
+        then = now.replace(hour=15, minute=1)
+        wait_time = (then - now).total_seconds()
+        await asyncio.sleep(7200)
 
+        channel = bot.get_channel(931040113146871822)
+
+        await channel.send("Eat fruits and stay healthy kivv")
+
+        
+async def schedule_daily_message3():
+    while True:
+        now = datetime.datetime.now()
+        #then = now+datetime.timedelta(days=0)
+        then = now.replace(hour=15, minute=1)
+        wait_time = (then - now).total_seconds()
+        await asyncio.sleep(18000)
+
+        channel = bot.get_channel(931040113146871822)
+
+        await channel.send("You are cute and mine kiv - mish")
+        
 @bot.event
 async def on_ready():
     print(f"Loggined in as: {bot.user.name}")
     await schedule_daily_message()
+    await schedule_daily_message2()
+    await schedule_daily_message3()
 
 
 
